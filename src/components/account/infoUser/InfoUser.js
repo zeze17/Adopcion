a3fddf446fb7aeeeadbba3104e5248522f47ca71
zeze17 <zeze0217@gmail.com>
2024-01-11 20:18:47 -0500

    avatar update

--- src/components/account/infoUser/InfoUser.js
+++ src/components/account/infoUser/InfoUser.js
@@ -1,16 +1,53 @@
-import React from 'react'
+import React , { useState } from 'react'
 import { View } from 'react-native'
 import {Avatar, Text} from 'react-native-elements';
-import {getAuth } from "firebase/auth";
+import {getAuth, updateProfile } from "firebase/auth";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import {styles} from "./InfoUser.styles"
+import * as ImagePicker from "expo-image-picker"
 
-export function InfoUser() {
+export function InfoUser(props) {
 
+  const {setloading, setloadingText} = props;
   const {uid, photoURL, displayName,email} = getAuth().currentUser
+  const [avatar, setAvatar] = useState(photoURL);
 
-  const cambiarAvatar = () =>{
-    console.log("cambiar avatar")
-  }
+
+  const cambiarAvatar = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      madiaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+  
+    if (!result.canceled) uploadImage(result.assets[0].uri);
+  };
+
+  const uploadImage = async (uri) => {
+    setloadingText("Actualizando Avatar");
+    setloading(true);
+
+    const response = await fetch(uri);
+    const blob = await response.blob();
+    const storage = getStorage();
+    const storageRef = ref(storage, `avatar/${uid}`);
+    console.log("esta es  la respuesta del storageRef" + storageRef);
+
+    uploadBytes(storageRef, blob).then((snapshot) => {
+      updatePhotoURL(snapshot.metadata.fullPath);     
+    });
+  };
+
+  const updatePhotoURL = async (imagePath) => {
+
+    const storage = getStorage();
+    const imageRef = ref(storage, imagePath);
+    const imageURL = await getDownloadURL(imageRef);
+    const auth = getAuth();
+    updateProfile(auth.currentUser, {photoURL : imageURL})
+    setAvatar(imageURL)
+    setloading(false);
+  };
 
   return (
     <View style={styles.content}>
@@ -18,15 +55,15 @@ export function InfoUser() {
       size="large" 
       rounded 
       containerStyle={styles.avatar}
-      icon={{type: "material", name: "person"}}
-      //source={{uri:photoURL}}
+      icon={{ type: "material", name: "person" }}
+      source={{ uri: avatar }}
       >
         <Avatar.Accessory size={24} onPress={cambiarAvatar} />
       </Avatar>
 
       <View>
         <Text style={styles.displayName}>{displayName || "Anonimo"}</Text>
-        <Text style={styles.content}>{email}</Text>
+        <Text> {email}</Text>
       </View>
     </View>
   )
--- src/screens/cuenta/userLoggerScreen/UserLoggerScreen.js
+++ src/screens/cuenta/userLoggerScreen/UserLoggerScreen.js
@@ -1,26 +1,32 @@
-import React from 'react'
-import { View } from 'react-native'
-import { Button} from 'react-native-elements'
-import {InfoUser} from "../../../components/account"
-import {getAuth, signOut} from 'firebase/auth'
-import { styles } from './UserLoggerScreen.styles'
+import React, { useState } from "react";
+import { View } from "react-native";
+import { Button } from "react-native-elements";
+import { InfoUser } from "../../../components/account";
+import { LoadingModal } from "../../../components";
+import { getAuth, signOut } from "firebase/auth";
+import { styles } from "./UserLoggerScreen.styles";
 
 export function UserLoggerScreen() {
-
+  const [loading, setloading] = useState(false);
+  const [loadingText, setloadingText] = useState('');
+  
   const logOut = async () => {
     const auth = getAuth();
     await signOut(auth);
-  }
+  };
 
   return (
     <View style={styles.content}>
-        <InfoUser/>
+      <InfoUser setloading={setloading} setloadingText={setloadingText} />
 
-        <Button title="Cerrar Sesion"
-         buttonStyle={styles.button}
-          titleStyle={styles.btnTitles}
-          onPress={logOut}
-         />
+      <Button
+        title="Cerrar Sesion"
+        buttonStyle={styles.button}
+        titleStyle={styles.btnTitles}
+        onPress={logOut}
+      />
+
+      <LoadingModal show={loading} text={loadingText} />
     </View>
-  )
+  );
 }
